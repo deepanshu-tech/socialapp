44f121fcb7480dca9b01b70bf4cb9d83ca557b5f
Naveen Kumar Jain <you@example.com>
2022-04-28 12:16:11 +0530

    added post api

--- routes/posts.js
+++ routes/posts.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const router = express.Router();
 const PostSchema = require('../models/posts');
+const UserSchema = require('../models/user')
 const mongoose = require('mongoose');
+const { route } = require('./users');
 const Post = mongoose.model("post" , PostSchema);
+const User = mongoose.model("user" , UserSchema);
 
 router.get("/" , (req,res)=>{
     res.send("This is post page!")
+})
+
+router.get("/:id" , async (req,res)=>{
+    try{
+        let post = await Post.findById(req.params.id);
+        res.status(200).json(post);
+    }
+    catch(err){
+        res.status(404).json(err);
+    }
 })
 
 //Create Post
@@ -21,22 +34,88 @@ router.post("/" , async(req,res)=>{
     }
 })
 
-router.post("/" , async(req,res)=>{
-    let newpost = new Post(req.body);
+router.post("/update/:postid" , async(req,res)=>{
+   
     try{
-        let savedpost = await newpost.save();
-        res.status(200).json(savedpost);
+        let post = await Post.findById(req.params.postid);
+        if(post.userId === req.body.userId){
+            try{
+               
+                let updatedpost = await Post.findByIdAndUpdate(req.params.postid , {$set:req.body});
+                res.status(500).json("User Updated");
+            }
+            catch(err){
+                res.status(500).json(err);
+            }
+        }
+    
+    }
+    catch(err){
+        res.status(500).json(err);
+    }
+    
+})
+//Delete Post
+router.delete("/delete/:postid" , async(req,res)=>{
+   
+    try{
+        let post = await Post.findById(req.params.postid);
+        if(post.userId === req.body.userId){
+            try{
+                let updatedpost = await Post.findByIdAndUpdate(req.params.postid , {$set:{"isDel":true}});
+                res.status(500).json("User Deleted");
+            }
+            catch(err){
+                res.status(500).json(err);
+            }
+        }
+    
+    }
+    catch(err){
+        res.status(500).json(err);
+    }
+    
+})
+
+//Like Post
+router.post("/:postid/react" , async(req , res)=>{
+    try{
+        let post = await Post.findById(req.params.postid);
+        if(!post.likes.includes(req.body.userId))
+        {
+            await post.updateOne({$push:{likes:req.body.userId}});
+            res.status(200).json("Post Liked!")
+        }
+        else{
+            await post.updateOne({$pull:{likes:req.body.userId}});
+            res.status(200).json("Post Disliked!")
+        }
     }
     catch(err){
         res.status(500).json(err);
     }
 })
+//Post posts on the Feed
+router.post('/feed' , async (req,res)=>{
+    try{
+        let feedPosts=[];
+        let currentuser = await User.findById(req.body.userId);
+        
+        let currentuserposts = await Post.find({"userId":req.body.userId});
+        
+        feedPosts = feedPosts.concat(...currentuserposts);
+        
 
-//Delete Post
-
-//Like Post
-
-// Unlike Post
-
-//Comment on Post
+        let myfeed = await Promise.all(
+            currentuser.following.map((friendId)=>{
+                return Post.find({"userId":friendId});
+            }));
+        console.log(feedPosts);
+        feedPosts = feedPosts.concat(...myfeed);
+        res.status(200).json(feedPosts);
+    }
+    catch(err){
+        res.status(500).json(err);
+    }
+})
 module.exports = router;
